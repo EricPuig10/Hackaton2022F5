d03af8e8929a0a102a05055b084946331cf9ffa6
eperez045 <eperez045@gmail.com>
2022-07-21 12:50:02 +0200

    filtro categorias

--- src/components/FilterForCategory/FilterForCategory.jsx
+++ src/components/FilterForCategory/FilterForCategory.jsx
@@ -1,7 +1,54 @@
-
+import { useEffect, useState } from "react";
+import { productServices } from "../../services/productServices";
+import { ProductsList } from "../ProductsList/ProductsList";
 
 export const FilterForCategory = () =>{
+    const [filtredProducts, setFiltredProducts] = useState(null);
+    useEffect(() => {
+        setFiltredProducts(productServices.getAllProducts());
+    }, []);
 
-    return 
+  function handleProduct(e) {
+    let typeProduct = e.target.value;
+    typeProduct !== "todo"
+      ? setFiltredProducts(productServices.filterProduct(typeProduct))
+      : setFiltredProducts(productServices.getAllProducts());}
+
+      const buttons = [
+        {
+          name: "Todo",
+          value: "todo"
+        },
+        {
+          name: "Pinturas",
+          value: "pinturas"
+        },
+        {
+          name: "Escultura",
+          value: "escultura"
+        },
+        {
+          name: "Fotografias",
+          value: "fotografias"
+        }
+      ];
+
+    return (
+        <>
+        {buttons &&
+        buttons.map((type, index) => (
+          <>
+            <button key={index} value={type.value} onClick={handleProduct}>
+              {type.name}
+            </button>
+          </>
+        ))}
+
+      {filtredProducts &&
+        filtredProducts.map(type => (
+          <ProductsList key={type.id} />
+        ))}
+        </>
+    )
     
 }
